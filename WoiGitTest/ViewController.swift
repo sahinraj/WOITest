860d120024a8a914fc23907c6bb6944a0b3d54b7
sahin.raj <sraj@gracenote.com>
2016-10-21 13:21:23 +0530

    Third Commit

--- WoiGitTest/ViewController.swift
+++ WoiGitTest/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Git First"
+        self.title = "Git Second"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
